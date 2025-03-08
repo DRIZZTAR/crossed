2d41c98d63749e4bb67cf192cf98042c5900b4fc
DRIZZTAR <tyson_jeremy@hotmail.com>
2025-03-08 11:50:20 -0700

    first push, added crossbow and fps controls

--- src/App.js
+++ src/App.js
@@ -6,23 +6,25 @@ import ThreeModel from './components/ThreeModel.js';
 import BaseCharacter from './ui/BaseCharacter';
 
 function App() {
-  return (
-    <BaseScene>
-      <BaseBox text={false} position={[0, 0.5, 0]} args={[2, 1, 2]} color="red" />
-      <BaseBox text={false} position={[5, 1, 0]} args={[1.5, 2, 1.3]} color="orange" />
-      <BaseBox text={false} position={[0, 0.5, 5]} args={[3, 1, 1.3]} color="green" />
+	return (
+		<div className='app-container'>
+			<BaseScene>
+				<BaseBox text={false} position={[0, 0.5, 0]} args={[2, 1, 2]} color='red' />
+				<BaseBox text={false} position={[5, 1, 0]} args={[1.5, 2, 1.3]} color='orange' />
+				<BaseBox text={false} position={[0, 0.5, 5]} args={[3, 1, 1.3]} color='green' />
 
-      <BaseCharacter controls position={[0, 2, 0]} args={[0.5]} color="yellow" />
+				<BaseCharacter controls position={[0, 2, 0]} args={[0.5]} color='yellow' />
 
-      <ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[10, 0, -5]} />
-      <ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[0, 0, 10]} />
-      <ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[-10, 0, 5]} />
-      <ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[-5, 0, -5]} />
-      <ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[0, 0, -10]} />
-      <ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[10, 0, 5]} />
-      <Sky />
-    </BaseScene>
-  );
+				<ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[10, 0, -5]} />
+				<ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[0, 0, 10]} />
+				<ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[-10, 0, 5]} />
+				<ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[-5, 0, -5]} />
+				<ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[0, 0, -10]} />
+				<ThreeModel args={[0.5, 2, 0.5]} scale={0.5} position={[10, 0, 5]} />
+				<Sky />
+			</BaseScene>
+		</div>
+	);
 }
 
 export default App;
--- src/ui/BaseScene.js
+++ src/ui/BaseScene.js
@@ -1,27 +1,28 @@
 import { Canvas } from '@react-three/fiber';
-import { Loader, PointerLockControls } from '@react-three/drei';
+import { Loader, PointerLockControls, Stats } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
 
 import Lights from '../components/Lights.js';
 import Floor from '../components/Floor.js';
 
 const BasicScene = ({ children }) => {
-  return (
-    <div>
-      <Canvas shadows camera={{ fov: 50 }}>
-        <Lights />
+	return (
+		<div className='scene-container'>
+			<Canvas shadows camera={{ fov: 50 }}>
+				<Stats />
+				<Lights />
 
-        <Physics gravity={[0, -9.8, 0]}>
-          {children}
+				<Physics gravity={[0, -9.8, 0]}>
+					{children}
 
-          <Floor rotation={[Math.PI / -2, 0, 0]} color="white" />
-        </Physics>
+					<Floor rotation={[Math.PI / -2, 0, 0]} color='white' />
+				</Physics>
 
-        <PointerLockControls />
-      </Canvas>
-      <Loader />
-    </div>
-  );
+				<PointerLockControls />
+			</Canvas>
+			<Loader />
+		</div>
+	);
 };
 
 export default BasicScene;
